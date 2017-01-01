8b50b256f2e1c4fdffdf1890e780d4bb7a731cb7
yuyang2009 <yuyang20091881@163.com>
2017-01-01 14:02:08 +0800

    chinanet

--- chinanet.py
+++ chinanet.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import print_function
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 import requests
 import time
 import os.path
@@ -13,13 +13,14 @@ def isLogin():
     # 通过post百度获得网络状态
     url_test = "https://www.baidu.com/"
     try:
-        login_code = requests.get(url_test, timeout=1).status_code
+        islogin_code = requests.get(url_test, timeout=5).status_code
     except requests.exceptions.ConnectionError:
-        print(time.ctime(), "未连接到chinanet！")
+        print(time.ctime(), u"未连接到chinanet！")
         return False
     except:
         return False
-    if int(x=login_code) == 200:
+
+    if int(islogin_code) == 200:
             return True
     else:
         return False
@@ -30,13 +31,13 @@ def getAccount():
     app_path = sys.path[0]
     file_path = os.path.join(os.path.abspath(app_path), file_name)
     if not os.path.exists(file_path):
-        account = input("请输入chinanet帐号 > ")
-        passwd = input("请输入chinanet密码 > ")
-        su_passwd = input("请输入sudo密码 > ")
+        account = input(u"请输入chinanet帐号 > ")
+        passwd = input(u"请输入chinanet密码 > ")
+        su_passwd = input(u"请输入sudo密码 > ")
         with open(file_path, "w") as f:
-            f.write("acount: %r\n" % account)
-            f.write("passwd: %r\n" % passwd)
-            f.write("su_passwd: %r\n" % su_passwd)
+            f.write("acount: %s\n" % account)
+            f.write("passwd: %s\n" % passwd)
+            f.write("su_passwd: %s\n" % su_passwd)
     else:
         with open(file_path, 'r') as f:
             account = f.readline().split(':')[-1].strip()
@@ -72,35 +73,35 @@ def Login(account, passwd):
             'select2': '-Select Service Provider-',
         }
     url_login = "http://wlan.ct10000.com/portal/login4V2.do"
-    print(time.ctime(), "正在尝试登录……")
+    print(time.ctime(), u"正在尝试登录……")
     try:
+        res = requests.post(url_login, data = postdata)
+    except:
         my_cmd = "echo ", su_passwd, "| sudo -S ifconfig en0 down && sudo ifconfig en0 up"   # might be wlan0
         my_cmd = ''.join(my_cmd)
         proc = Popen(my_cmd, shell=True)
-        res = requests.post(url_login, data = postdata)
-    except:
-        print(time.ctime(), "Login, 连接失败!")
+        print(time.ctime(), u"Login, 连接失败!")
 
 def Logout():
     url_logout = "http://wlan.ct10000.com/portal/Logout.do?NasType=Huawei&NasName=BJ-DS-SR-1.M.ME60"
     try:
         res = requests.get(url_logout)
-        print(time.ctime(), "logout, 已注销Chinanet帐号！")
+        print(time.ctime(), u"logout, 已注销Chinanet帐号！")
     except requests.exceptions.ConnectionError:
-        print(time.ctime(), "logout, 失败！")
+        print(time.ctime(), u"logout, 失败！")
 
 if __name__ == '__main__':
 
     account, passwd, su_passwd = getAccount()
     if isLogin():
-        print(time.ctime(), "已经连接到网络。")
+        print(time.ctime(), u"已经连接到网络。")
     while True:
         if not isLogin():
             #Logout()
             Login(account, passwd)
-            time.sleep(5)
+            time.sleep(10)
             if isLogin():
-                print(time.ctime(), "连接成功！")
+                print(time.ctime(), u"连接成功！")
             else:
-                print(time.ctime(), "连接失败，将在5秒后重试……")
-        #time.sleep(5)
+                print(time.ctime(), u"连接失败，将在5秒后重试……")
+        time.sleep(10)
